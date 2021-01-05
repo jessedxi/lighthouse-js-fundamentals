9019755b8681b3a7454671551e83a71dbd625f89
Jesse <jessedaniel@live.ca>
2021-01-05 19:05:35 +0000

    hello.js completed

--- hello.js
+++ hello.js
@@ -1,5 +1,22 @@
+// prints hello 'name' to console
 const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello("JD");
+
+// as above, prints hello to console
+const sayHelloToConsole = function (name) {
+  console.log("Hello " + name);
+}
+
+sayHelloToConsole("Daniel");
+
+// example using return
+const returnSayHello = function (name ) {
+  return "hello, " + name;
+}
+
+// function saved as variable
+const greeting = returnSayHello('John');
+console.log(greeting);
